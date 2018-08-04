3398f7eee361b8a8c266c88fdde68cd328410b7c
bigblobby <tom_dempster@hotmail.co.uk>
2018-08-04 19:27:07 +0100

    Added delete buttons, updated styling

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,7 +10,7 @@ const valueInput = document.getElementById('value-input');
 const totalAmount = document.getElementById('total');
 
 // Select ul
-
+const output = document.getElementById('output');
 const incomeUL = document.getElementById('income-output');
 const expensesUL = document.getElementById('expenses-output');
 
@@ -20,10 +20,9 @@ let expenses = 0;
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  let selectValue = selectInput.value;    // Selects income OR expenses
-  let descValue = descInput.value;        // Selects description
-  let valueInputValue = Number(valueInput.value); // Selects amount entered
-  console.log(valueInputValue);
+  let selectValue = selectInput.value;            // Select income OR expenses
+  let descValue = descInput.value;                // Gets description
+  let valueInputValue = Number(valueInput.value); // Gets amount entered
   if (selectValue === 'income' && isFinite(valueInputValue) && descValue !== '' && valueInputValue) {
     // Update income amount and main display
     income += valueInputValue;
@@ -39,31 +38,65 @@ form.addEventListener('submit', (e) => {
     totalAmount.textContent = formatTotal();
     clearInput();
   }
-
-  function formatMoney(type) {
-    return '£' + type.toFixed(2);
-  }
-
-  function formatTotal() {
-    let total = income - expenses;
-    return '£' + total.toFixed(2);
-  }
-
-  function addReason(info, ul, money) {
-    const li = document.createElement('li');
-    const infoSpan = document.createElement('span');
-    const moneySpan = document.createElement('span');
-    infoSpan.textContent = info;
-    moneySpan.textContent = formatMoney(money);
-    li.append(infoSpan);
-    li.append(moneySpan);
-    ul.append(li);
-  }
-
-  function clearInput(desc, value) {
-    descInput.value = '';
-    valueInput.value = '';
-  }
-
 });
 
+output.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    if (e.target.parentElement.parentElement.id === 'income-output') {
+
+      // Update display
+      const tempAmount = e.target.previousElementSibling.textContent;
+      income -= tempAmount.slice(1, tempAmount[length - 1]);
+      displayIncome.textContent = formatMoney(income);
+      totalAmount.textContent = formatTotal();
+
+      // Delete Item
+      removeItem(e);
+
+    } else if (e.target.parentElement.parentElement.id === 'expenses-output') {
+
+      // Update display
+      const tempAmount = e.target.previousElementSibling.textContent;
+      expenses -= tempAmount.slice(1, tempAmount[length - 1]);
+      displayExpenses.textContent = formatMoney(expenses);
+      totalAmount.textContent = formatTotal();
+
+      // Delete Item
+      removeItem(e);
+    }
+  }
+});
+
+function formatTotal() {
+  let total = income - expenses;
+  return '£' + total.toFixed(2);
+}
+
+function formatMoney(type) {
+  return '£' + type.toFixed(2);
+}
+
+function addReason(info, ul, money) {
+  const li = document.createElement('li');
+  const infoSpan = document.createElement('span');
+  const moneySpan = document.createElement('span');
+  const button = document.createElement('button');
+  button.textContent = 'X';
+  infoSpan.textContent = info;
+  moneySpan.textContent = formatMoney(money);
+  li.append(infoSpan);
+  li.append(moneySpan);
+  li.append(button);
+  ul.append(li);
+}
+
+function clearInput(desc, value) {
+  descInput.value = '';
+  valueInput.value = '';
+}
+
+function removeItem(event) {
+  const ul = event.target.parentElement.parentElement;
+  const li = event.target.parentElement;
+  ul.removeChild(li);
+}
