974ad6db62381a9282d0157d3fdbcee85d16c7f7
bigblobby <tom_dempster@hotmail.co.uk>
2018-08-04 12:44:55 +0100

    Refactored, added more features

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,8 +5,14 @@ const displayExpenses = document.getElementById('display-expenses');
 // Select form controls
 const form = document.getElementById('form-input');
 const selectInput = document.getElementById('select-input');
+const descInput = document.getElementById('desc-input');
 const valueInput = document.getElementById('value-input');
 const totalAmount = document.getElementById('total');
+
+// Select ul
+
+const incomeUL = document.getElementById('income-output');
+const expensesUL = document.getElementById('expenses-output');
 
 // Set up variables
 let income = 0;
@@ -14,26 +20,50 @@ let expenses = 0;
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  let total;
-  if (selectInput.value === 'income' && isFinite(valueInput.value)) {
+  let selectValue = selectInput.value;    // Selects income OR expenses
+  let descValue = descInput.value;        // Selects description
+  let valueInputValue = Number(valueInput.value); // Selects amount entered
+  console.log(valueInputValue);
+  if (selectValue === 'income' && isFinite(valueInputValue) && descValue !== '' && valueInputValue) {
     // Update income amount and main display
-    income += Number(valueInput.value);
-    displayIncome.textContent = formatIncomeAndExpenses(income);
+    income += valueInputValue;
+    displayIncome.textContent = formatMoney(income);
+    addReason(descValue, incomeUL, valueInputValue);
     totalAmount.textContent = formatTotal();
-  } else if (selectInput.value === 'expense' && isFinite(valueInput.value)) {
+    clearInput();
+  } else if (selectValue === 'expense' && isFinite(valueInputValue) && descValue !== '' && valueInputValue) {
     // Update expense amount and main display
-    expenses += Number(valueInput.value);
-    displayExpenses.textContent = formatIncomeAndExpenses(expenses);
+    expenses += valueInputValue;
+    displayExpenses.textContent = formatMoney(expenses);
+    addReason(descValue, expensesUL, valueInputValue);
     totalAmount.textContent = formatTotal();
+    clearInput();
   }
 
-  function formatIncomeAndExpenses(type) {
+  function formatMoney(type) {
     return '£' + type.toFixed(2);
   }
 
   function formatTotal() {
-    total = income - expenses;
+    let total = income - expenses;
     return '£' + total.toFixed(2);
   }
+
+  function addReason(info, ul, money) {
+    const li = document.createElement('li');
+    const infoSpan = document.createElement('span');
+    const moneySpan = document.createElement('span');
+    infoSpan.textContent = info;
+    moneySpan.textContent = formatMoney(money);
+    li.append(infoSpan);
+    li.append(moneySpan);
+    ul.append(li);
+  }
+
+  function clearInput(desc, value) {
+    descInput.value = '';
+    valueInput.value = '';
+  }
+
 });
 
